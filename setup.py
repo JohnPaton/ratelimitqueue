05898a17f813765fe315af277763e237ee50bda4
John Paton <john@johnpaton.net>
2018-08-11 12:27:05 +0200

    Blacked setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
 
-with open('README.md', 'r') as h:
+with open("README.md", "r") as h:
     README = h.read()
 
 
 setup(
-    name='ratelimitqueue',
-    version='0.1.0',
-    description='A thread safe, rate limited queue.',
+    name="ratelimitqueue",
+    version="0.1.0",
+    description="A thread safe, rate limited queue.",
     long_description=README,
     long_description_content_type="text/markdown",
-    url='https://www.github.com/JohnPaton/ratelimitqueue',
-    author='John Paton',
-    author_email='john@johnpaton.net',
-    python_requires='>=3.4',
-    packages=['ratelimitqueue'],
+    url="https://www.github.com/JohnPaton/ratelimitqueue",
+    author="John Paton",
+    author_email="john@johnpaton.net",
+    python_requires=">=3.4",
+    packages=["ratelimitqueue"],
 )
